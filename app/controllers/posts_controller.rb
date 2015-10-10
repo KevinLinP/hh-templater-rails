f02096311fe2672a4d0707bf7bd6116a1e7c722c
Kevin Lin <kevin.lin.p@gmail.com>
2015-10-09 23:14:37 -0700

    Starting posts#create.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,31 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+
+    render :new
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    if @post.persisted?
+      redirect_to post_path(post)
+    else
+      render action: :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(
+      :email_subject, :venue_name,
+      :location_name, :short_address, :start_time, :body,
+      :maps_url, :header_image_url, :footer_image_url
+    )
   end
 
 end
